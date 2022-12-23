52aab3b7b81ca0dd6b828ac075a9be83e3b45ba8
unknown <jacobzff77@gmail.com>
2022-12-23 22:09:04 +0100

    random vr

--- main.js
+++ main.js
@@ -3,15 +3,44 @@
 console.log("console.log output tst");
 
 
-let admin;
-let name;
+const myInt = 5;
+const myFloat = 2.123456789;
 
-name = "John";
-admin  = name;
 
-alert(admin);
+console.log(myInt)
+console.log(myFloat)
 
-let x = 0.1;
-let y = 0.2;
 
-console.log(x+y);
+const twoDecimalPlaces = myFloat.toFixed(2);
+
+console.log(twoDecimalPlaces);
+
+
+let stringNumber = "75";
+
+stringNumber = Number(stringNumber);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
+
+let a = 5;
+let b = "5";
+
+console.log(a==b);
+
+let valueOne = 5;
+let valueTwo = "4";
+
+console.log(+valueOne + +valueTwo)
+
+let n = 2;
+
+n *= 3 + 5;
+
+console.log(n);
+
+
+let at = 10;
+
+console.log(at*9);
