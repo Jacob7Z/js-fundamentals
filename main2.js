2c7cf2865e5776a77314f6545e2898eebd385e45
unknown <jacobzff77@gmail.com>
2022-12-24 12:43:31 +0100

    strings data types numbers

---
+++ main2.js
@@ -0,0 +1,38 @@
+const bigint = 5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555n;
+
+// document.write(bigint.toLocaleString());
+console.log(bigint.toLocaleString());
+
+
+let userName = "moe";
+
+console.log(`supsup ${userName}`);
+
+let compare = 4 < 5 // returns true or false 
+
+console.log(compare);
+
+let undefinedValue;
+
+console.log(undefinedValue);
+
+//using ' in a '' string
+
+let text = 'I\'ve done nothing'
+
+console.log(text);
+
+
+let textToNextLine = `yo this is a text that breaks here.
+ we are in the underworld boi using \`\` `;
+
+console.log(textToNextLine);
+
+
+let textToNextLine2 = "this is another text that breaks here.\n yeeted to the next line using \\n";
+
+console.log(textToNextLine2);
+
+
+
+
