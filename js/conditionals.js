4ed460607cba159607b71fe5966d6caf234b770d
unknown <jacobzff77@gmail.com>
2022-12-28 10:37:00 +0100

    check the login exercise

--- js/conditionals.js
+++ js/conditionals.js
@@ -84,3 +84,29 @@ if(age < 14 || age > 90){
 
 
 
+
+let promptMessage = prompt("who is there");
+
+if( promptMessage === "admin"){
+    
+    let pass = prompt("Password?");
+    
+    if(pass === "pass"){
+        alert("welcome!");
+    }else if(pass === ' ' || pass === null){
+        alert("Canceled");
+    }else{
+        alert("wrong password");
+    }
+    
+    console.log(pass);
+
+}else if(promptMessage === ' ' || promptMessage === null){
+    alert("Canceled");
+}else{
+    alert("I dont know you");
+}
+
+console.log(promptMessage);
+
+
